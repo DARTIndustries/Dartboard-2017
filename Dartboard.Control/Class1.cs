bb059ce81eb9a85c1f404a4eaa78e977104f45e8
Adam Schiavone <adam8797@me.com>
2017-07-17 15:47:04 -0400

    Adjusted all namespaces

--- Dartboard.Control/Class1.cs
+++ Dartboard.Control/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dartboard.Control
+﻿namespace DART.Dartboard.Control
 {
     public class Class1
     {

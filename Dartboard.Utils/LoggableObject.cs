0b25e3d9f5d1d5e92fa472557a6e4bc7cc376e3f
Adam Schiavone <adam8797@me.com>
2017-07-22 12:42:16 -0400

    Added Utils Project

---
+++ Dartboard.Utils/LoggableObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+
+namespace Dartboard.Utils
+{
+    public abstract class LoggableObject
+    {
+        protected ILog Log;
+
+        protected LoggableObject()
+        {
+            Log = LogManager.GetLogger(GetType());
+        }
+    }
+}

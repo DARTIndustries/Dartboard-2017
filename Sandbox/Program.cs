b9bacd45b2c4d632658a05a9c84018581f717592
Adam Schiavone <adam8797@me.com>
2017-08-26 17:01:55 -0400

    Sandbox

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using DART.Dartboard.HID;
+using System.Net.Sockets;
+using Dartboard.Utils;
 
 namespace Sandbox
 {
@@ -11,17 +14,58 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            var wrapper = new JoystickWrapper(DeviceType.FirstPerson);
-            if (wrapper.Acquired)
+            var manager = HIDManager.SharedManager;
+
+            while (true)
             {
-                while (true)
-                {
-                    wrapper.Poll();
-                    Console.WriteLine(wrapper._currentState.X);
-                }
+                var state1 = manager.GetGamepadState();
+                var state2 = manager.GetJoystickState();
+                PropPrint(state1);
+                PropPrint(state2);
+                Thread.Sleep(50);
+                Console.Clear();
+            }
+        }
+
+
+        static void PropPrint(object o)
+        {
+            if (o == null)
+                return;
+
+            var t = o.GetType();
+            Console.WriteLine(t.Name);
+            var fields = t.GetFields();
+            foreach (var fieldInfo in fields)
+            {
+                Console.WriteLine(fieldInfo.Name + ": " + fieldInfo.GetValue(o));
             }
 
-            Console.ReadLine();
+            var props = t.GetProperties();
+            foreach (var propertyInfo in props)
+            {
+                if (propertyInfo.PropertyType == typeof(int[]))
+                {
+                    Console.WriteLine(propertyInfo.Name + ": " + string.Join(", ", (int[])propertyInfo.GetValue(o)));
+                }
+                else if (propertyInfo.PropertyType == typeof(bool[]))
+                {
+                    var ba = (bool[]) propertyInfo.GetValue(o);
+                    if (ba.Length > 5)
+                    {
+                        Console.WriteLine(propertyInfo.Name + ": ");
+                        for (int i = 0; i < Math.Min(ba.Length, 15); i++)
+                        {
+                            Console.WriteLine("  {0}: {1}", i, ba[i]);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(propertyInfo.Name + ": " + propertyInfo.GetValue(o));
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
